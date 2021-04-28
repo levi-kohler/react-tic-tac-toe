c9ffd07efba12d2f5ea313eb47a0f1e426129bf7
Levi Kohler <kohler.la01@gmail.com>
2021-04-28 14:29:12 -0700

    learned why squares[a] is necessary at the beginning of conditional in line 112
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -97,7 +97,7 @@ ReactDOM.render(
 
 function calculateWinner(squares) {
   const lines = [
-    [0, 1, 3],
+    [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
@@ -106,6 +106,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
