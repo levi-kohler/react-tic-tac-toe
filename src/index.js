c02d8079f4f3d77f8ae5f8488e5f95395dc7342c
Levi Kohler <kohler.la01@gmail.com>
2021-04-28 14:49:19 -0700

    move handleClick function to Gam component and concat History of moves
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -14,30 +14,19 @@ function Square(props) {
   }
 
 class Board extends React.Component {  
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
+  
   renderSquare(i) {
     return (
       <Square
-        value={this.props.squares[i]} 
-        onClick={() => this.props.onClick(i)}
+      value={this.props.squares[i]} 
+      onClick={() => this.props.onClick(i)}
       />
-    );
-  }
-
-  render() {
-    return (
-      <div>
+      );
+    }
+    
+    render() {
+      return (
+        <div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -69,8 +58,24 @@ class Game extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
   render() {
-    const history = this.state.history:
+    const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
     let status;
